32a774b724148eb1bac9980b88dfb58a44fa3a98
PeterHo <peter45402@163.com>
2019-12-23 21:25:54 +0800

    Create ProductTest.java
    Create ProductTest class

---
+++ src/main/java/com/ensat/entities/ProductTest.java
@@ -0,0 +1,44 @@
+package com.ensat.entities;
+
+public class ProductTest {
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void setName() {
+    }
+
+    @org.junit.Test
+    public void getVersion() {
+    }
+
+    @org.junit.Test
+    public void setVersion() {
+    }
+
+    @org.junit.Test
+    public void getId() {
+    }
+
+    @org.junit.Test
+    public void setId() {
+    }
+
+    @org.junit.Test
+    public void getProductId() {
+    }
+
+    @org.junit.Test
+    public void setProductId() {
+    }
+
+    @org.junit.Test
+    public void getPrice() {
+    }
+
+    @org.junit.Test
+    public void setPrice() {
+    }
+}
